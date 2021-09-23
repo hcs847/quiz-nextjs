3ecbf911a23c44fe4c09322943a4a4a474f394f1
Hila Caspi <caspi.home@gmail.com>
2021-09-22 18:52:06 -0700

    initial components setup

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import headerStyles from '../styles/Header.module.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={headerStyles.title}>Javascript Quiz Home</h1>
+            <p className={headerStyles.description}>Try a quiz to test your Full Stack knowledge
+                <br />Retake it until you Ace it!</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const about = () => {
+    return (
+        <div>
+            <p>About page</p>
+        </div>
+    )
+}
+
+export default about
