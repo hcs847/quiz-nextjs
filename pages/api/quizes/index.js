08b49ec2c25a37e8f26f99c211a3f9a11a1c1d3a
Hila Caspi <caspi.home@gmail.com>
2021-09-29 20:29:56 -0700

    fix vercel deploy

--- pages/api/quizes/index.js
+++ pages/api/quizes/index.js
@@ -1,5 +1,17 @@
 import { quizes } from '/public/data';
 
-export default function handler(req, res) {
-    res.status(200).json(quizes)
+// import { server } from '../../../config';
+
+export const getQuizes = () => {
+    console.log('quizes api===', quizes)
+    return { data: quizes };
 }
+
+export default function handler(req, res) {
+    res.status(200).json(getQuizes());
+}
+// export const getData = async () => {
+//     const res = await fetch(`${server}/api/quizes`);
+//     const quizes = await res.json();
+//     return quizes;
+// }
