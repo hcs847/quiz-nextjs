9fc7a321b1aaf1a91ce336abf7d40696aff4d7bc
Hila Caspi <caspi.home@gmail.com>
2021-09-30 11:23:15 -0700

    styling adjustments home page

--- pages/api/quizes/index.js
+++ pages/api/quizes/index.js
@@ -1,17 +1,9 @@
 import { quizes } from '/public/data';
 
-// import { server } from '../../../config';
-
 export const getQuizes = () => {
-    console.log('quizes api===', quizes)
     return { data: quizes };
 }
 
 export default function handler(req, res) {
     res.status(200).json(getQuizes());
 }
-// export const getData = async () => {
-//     const res = await fetch(`${server}/api/quizes`);
-//     const quizes = await res.json();
-//     return quizes;
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import QuestionsList from '../components/QuestionsList';
-import { server } from '../config';
-
 import { getQuizes } from './api/quizes';
 
 export default function Home({ quizes }) {
   const [showQuestions, setShowQuestions] = useState({});
   const toggleQuestions = (subject) => {
     setShowQuestions({ ...showQuestions, [subject]: !showQuestions[subject] });
-    console.log(subject, showQuestions)
   }
 
   return (
@@ -21,33 +18,25 @@ export default function Home({ quizes }) {
       <div >
         <h3>quizes list</h3>
         {quizes.map(quiz => (
-          <div key={quiz.subject}>
-            <button type='button'
-              className='btn btn-modal'
-              onClick={() => toggleQuestions(quiz.subject)}
-            >{quiz.subject}</button>
+          <>
+            <div key={quiz.subject}>
+              <button type='button'
+                className='btn btn-modal'
+                onClick={() => toggleQuestions(quiz.subject)}
+              >{quiz.subject}</button>
+            </div>
             <div className={showQuestions[quiz.subject] ? 'question' : 'hidden'}>
               <QuestionsList quiz={quiz} />
             </div>
-          </div>
+          </>
         ))}
-
       </div>
     </>
   )
 }
 
 export const getServerSideProps = async () => {
-
-  // const res = await fetch(`${server}/api/quizes`);
-  // const quizes = await res.json();
-
   const quizes = getQuizes();
-
-  console.log('quizes from get props', quizes, typeof quizes);
-
-  // getData();
-
   if (!quizes) {
     return {
       notFound: true
