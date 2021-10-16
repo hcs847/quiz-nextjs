33cef64fc5b34d27b64be8f2d6de7a1309684fcd
Hila Caspi <caspi.home@gmail.com>
2021-10-15 22:53:17 -0700

    fix cors

--- pages/api/snippets/index.js
+++ pages/api/snippets/index.js
@@ -1,4 +1,24 @@
 import fs from 'fs';
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
 
 export const getSnippets = () => {
     const files = fs.readdirSync('snippets');
@@ -8,6 +28,10 @@ export const getSnippets = () => {
     return snippets;
 }
 
-export default function handler(req, res) {
+async function handler(req, res) {
+    // Run the middleware
+    await runMiddleware(req, res, cors);
     res.status(200).json(getSnippets());
 }
+
+export default handler;
