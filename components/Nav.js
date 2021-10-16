08de6e4a36df9d2993127c2caedc922cbd40de95
Hila Caspi <caspi.home@gmail.com>
2021-10-15 23:32:22 -0700

    fix cors

--- components/Nav.js
+++ components/Nav.js
@@ -15,6 +15,7 @@ export default function Nav() {
 
     useEffect(() => {
         getSnippets();
+        console.log(getSnippets(), "useEffect")
     }, [])
 
     return (
--- pages/api/snippets/index.js
+++ pages/api/snippets/index.js
@@ -1,4 +1,25 @@
 import fs from 'fs';
+import Cors from 'cors';
+
+// Initializing the cors middleware
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            console.log(result, "===result")
+            if (result instanceof Error) {
+                return reject(result)
+            }
+
+            return resolve(result)
+        })
+    })
+}
 
 export const getSnippets = () => {
     const files = fs.readdirSync('snippets');
@@ -9,7 +30,8 @@ export const getSnippets = () => {
 }
 
 async function handler(req, res) {
-    console.log(getSnippets());
+    // Run the middleware
+    await runMiddleware(req, res, cors);
     res.status(200).json(getSnippets());
 }
 
